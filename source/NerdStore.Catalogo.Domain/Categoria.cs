df7757a644152c4a6d399b51284e700a3c98e973
ddvieira <dviiera.dantas98@gmail.com>
2022-07-21 00:02:54 -0300

    Implementing Application Layer

--- source/NerdStore.Catalogo.Domain/Categoria.cs
+++ source/NerdStore.Catalogo.Domain/Categoria.cs
@@ -7,20 +7,29 @@ namespace NerdStore.Catalogo.Domain
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
-        public IList<Produto> Produtos { get; private set; }
+
+        // EF Relation
+        public ICollection<Produto> Produtos { get; set; }
 
         protected Categoria() { }
 
-        public Categoria(string nome, int codigo, IList<Produto> produtos)
+        public Categoria(string nome, int codigo)
         {
             Nome = nome;
             Codigo = codigo;
-            Produtos = produtos;
+
+            Validar();
         }
 
         public override string ToString()
         {
             return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
         }
     }
 }
